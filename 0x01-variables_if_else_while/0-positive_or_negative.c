f567a1da2b429377f5cfa78e945e52342a0d397f
elmostakun <elmostapha.dounia@gmail.com>
2023-06-05 01:47:31 -0700

    pos\neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,13 +18,13 @@ if (n > 0)
 {
 printf("%i is positive\n", n);
 }
-else if (n < 0)
+else if (n == 0)
 {
-printf("%i is negative\n", n);
+printf("%i is zero\n", n);
 }
 else
 {
-printf("%i is zero", n);
+printf("%i is negative", n);
 }
 return (0);
 }
