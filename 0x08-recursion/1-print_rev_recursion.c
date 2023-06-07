39fdbac890c00a76cf1e4c8bb9e4de8d9d4a12b1
elmostakun <elmostapha.dounia@gmail.com>
2023-06-07 09:07:16 -0700

    recur

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - function to print a string in reverse
+ * @s: a string
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
