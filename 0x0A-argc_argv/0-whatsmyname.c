cb94c4d96fcf5e97021323b92a65bade26f6ad7f
elmostakun <elmostapha.dounia@gmail.com>
2023-06-09 14:30:45 -0700

    void argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the main function
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int __attribute__((unused)) fvoid = argc;
+printf("%s\n", argv[0]);
+return (0);
+}
