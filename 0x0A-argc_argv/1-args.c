866c2bb28f0c80a2479c65cd849a9ecbc81a0e06
elmostakun <elmostapha.dounia@gmail.com>
2023-06-09 14:38:42 -0700

    void argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+char *__attribute__((unused)) voided = argv[0];
+printf("%d\n", argc);
+return (0);
+}
