1e901d4cb1a99f99d54a0aa9b1bde4f0b02b60cf
elmostakun <elmostapha.dounia@gmail.com>
2023-06-11 09:21:40 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,18 +12,15 @@
 int main(int argc, char *argv[])
 {
 int res, num1, num2;
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-res = num1 *num2;
+
 if (argc < 3 || argc > 3)
 {
 printf("Error\n");
 return (1);
 }
-
-
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+res = num1 *num2;
 printf("%d\n", res);
-
-
 return (0);
 }
