06892dbdd3640a6011afb44147250a6e09f8c566
elmostakun <elmostapha.dounia@gmail.com>
2023-06-11 09:18:47 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - the main function
+ * @argc: the count
+ * @argv: the string
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int res, num1, num2;
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+res = num1 *num2;
+if (argc < 3 || argc > 3)
+{
+printf("Error\n");
+return (1);
+}
+
+
+printf("%d\n", res);
+
+
+return (0);
+}
