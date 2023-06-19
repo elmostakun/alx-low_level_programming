0782662057a2d83a8b42683a329002734940c954
elmostakun <elmostapha.dounia@gmail.com>
2023-06-19 12:10:53 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,12 @@
 #define STRUCTERS_TYPEDEF_H
 #include <stdio.h>
 
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
 struct dog
 {
 char *name;
